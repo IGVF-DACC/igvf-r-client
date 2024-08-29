3da2d7bd598d19ab7251c4259f1f7f01b110dab1
Keenan Graham <ksgraham@stanford.edu>
2024-08-28 17:54:55 -0700

    Update tests

--- tests/test_endpoints.R
+++ tests/test_endpoints.R
@@ -4,8 +4,8 @@ library("testthat")
 
 test_that("IGVF API search returns expected response", {
   api <- IgvfApi$new()
-  response <- api$Search(query="abc", limit=1, type=cat("Software"))
-  expect_true(response$total > 2)
+  response <- api$Search(query="abc", limit=1, type=c("Software"))
+  expect_true(response$total >= 2)
   expect_true(length(response$`@graph`) == 1)
   software <- response$`@graph`[[1]]$actual_instance
   expect_equal(software$title, "ABC")
